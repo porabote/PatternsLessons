2478afef7dfce5d20b45a50af784b8312a35374d
maksimov <dmaksimov@vtb.ru>
2023-04-03 21:22:38 +0300

    builder

---
+++ person/src/main/java/Person.java
@@ -0,0 +1,68 @@
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected String address;
+    protected Integer age;
+
+    protected PersonBuilder child;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return (this.age != null) ? true : false;
+    }
+
+    public boolean hasAddress() {
+        return (this.address != null) ? true : false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public Integer getAge() {
+        return this.hasAge() ? this.age : null;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        this.age++;
+    }
+
+    @Override
+    public String toString() {
+        return "User" + this.name;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return this.child = new PersonBuilder()
+                .setSurname(this.surname)
+                .setAge(-100)
+                .setAddress(this.address);
+    }
+}
---
+++ person/src/main/java/PersonBuilder.java
@@ -0,0 +1,46 @@
+public class PersonBuilder {
+
+    protected String name;
+    protected String surname;
+    protected String address;
+    protected Integer age;
+
+    public PersonBuilder PersonBuilder() {
+        return this;
+    }
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() throws IllegalStateException {
+
+        if (this.age < 0) {
+            throw new IllegalStateException("Age must be more or equals than 0");
+        } else if (this.name == null || this.surname == null) {
+            throw new IllegalStateException("Person must has name and surname");
+        }
+
+        if (this.age == null) {
+            return new Person(this.name, this.surname);
+        } else {
+            return new Person(this.name, this.surname, this.age);
+        }
+    }
+}
